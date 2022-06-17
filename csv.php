71b6c5de38792c97f1107c7a52051d6e4bd55f57
Azziden <95377491+Azziden@users.noreply.github.com>
2022-06-17 11:25:54 +0200

    Update csv.php

--- csv.php
+++ csv.php
@@ -36,7 +36,7 @@ class csv
             $chunked = array_chunk($data, 30);
 
             for($i = 0; $i < count($chunked); $i++) {
-                $sql = "INSERT INTO `magazine` (`id`, `type`, `something`, `something`, `date`)
+                $sql = "INSERT INTO `magazine` (`id`, `something`, `something`, `something`, `something`)
                 VALUES";
 
                 for ($j=0; $j < count($chunked[$i]); $j++) { 
